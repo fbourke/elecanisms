935d712809a84434ae2d7e367c31ac37aa684ece
Forrest Bourke <forrestbourke@gmail.com>
2015-01-22 12:15:24 -0500

    makes a binary counter

--- blink/blink.c
+++ blink/blink.c
@@ -11,16 +11,29 @@ int16_t main(void) {
 
     led_on(&led1);
     led_on(&led2);
-    timer_setPeriod(&timer2, 0.5);
+    led_on(&led3);
+    timer_setPeriod(&timer1, 0.125);
+    timer_start(&timer1);
+    timer_setPeriod(&timer2, 0.25);
     timer_start(&timer2);
+    timer_setPeriod(&timer3, 0.5);
+    timer_start(&timer3);
 
     while (1) {
-        if (timer_flag(&timer2)) {
-            timer_lower(&timer2);
+        if (timer_flag(&timer1)) {
+            timer_lower(&timer1);
             led_toggle(&led1);
         }
-        led_write(&led2, !sw_read(&sw2));
-        led_write(&led3, !sw_read(&sw3));
+        if (timer_flag(&timer2)) {
+            timer_lower(&timer2);
+            led_toggle(&led2);
+        }
+        if (timer_flag(&timer3)) {
+            timer_lower(&timer3);
+            led_toggle(&led3);
+        }
+        //led_write(&led2, !sw_read(&sw2));
+        //led_write(&led3, !sw_read(&sw3));
     }
 }
 
