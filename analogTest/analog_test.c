7ce7f8d9f1e46f492bc29fb94df0171a2d34f247
Eric Schneider <franz.eric.schneider@gmail.com>
2015-01-31 17:18:16 -0500

    Successful analogTest, empty pwmTest

--- analogTest/analog_test.c
+++ analogTest/analog_test.c
@@ -1,6 +1,7 @@
 #include <p24FJ128GB206.h>
 #include "config.h"
 #include "common.h"
+#include "timer.h"
 #include "uart.h"
 #include <stdio.h>
 
@@ -8,7 +9,17 @@ uint8_t string[40];
 
 int16_t main(void) {
     init_clock();
+    init_pin();
+    init_timer();
     init_uart();
+
+    timer_setPeriod(&timer1, 0.5);
+    timer_start(&timer1);
+
+    int i;
+    for (i = 0; i<6; i++) {
+        pin_analogIn(&A[i]);
+    }
 
     printf("Hello World!\n");
 
@@ -16,11 +27,16 @@ int16_t main(void) {
     uart_gets(&uart1, string, 40);
     printf("Hello %s!\n", string);
 
-    printf("Type something at the prompt.\n");
+    printf("Printing analog voltages.\n");
 
     while (1) {
-        printf(">> ");
-        uart_gets(&uart1, string, 40);
-        printf("You typed  '%s'\n", string);
+        if (timer_flag(&timer1)) {
+            timer_lower(&timer1);
+            for (i = 0; i<6; i++) {
+                pin_analogIn(&A[i]);
+                printf(">> Voltage: %d\n", pin_read(&A[i]) >> 6);
+            }
+            printf("\n");
+        }
     }
 }
--- pwm_test/pwm_test.c
+++ pwm_test/pwm_test.c
@@ -1,6 +1,9 @@
 #include <p24FJ128GB206.h>
 #include "config.h"
 #include "common.h"
+#include "oc.h"
+#include "pin.h"
+#include "timer.h"
 #include "uart.h"
 #include <stdio.h>
 
@@ -8,7 +11,13 @@ uint8_t string[40];
 
 int16_t main(void) {
     init_clock();
-    init_uart();  // Default is 19200 baud
+    init_oc();
+    init_pin();
+    init_timer();
+    init_uart();
+
+// void oc_pwm(_OC *self, _PIN *out, _TIMER *timer, float freq (Hz), uint16_t duty)
+    oc_pwm(&oc[0], &D[0], NULL, 10000, 512 << 6);
 
     printf("Hello World!\n");
 
@@ -16,12 +25,9 @@ int16_t main(void) {
     uart_gets(&uart1, string, 40);
     printf("Hello %s!\n", string);
 
-    printf("Type something at the prompt.\n");
+    printf("Testing PWM.\n");
 
     while (1) {
-        printf(">> ");
-        uart_gets(&uart1, string, 40);
-        printf("You typed '%s'\n", string);
+        // Change the PWM signal here
     }
 }
-
