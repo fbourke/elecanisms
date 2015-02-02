f41b0dbee0d060187c0a89bf87b71a1c2fda57f8
Forrest Bourke <forrestbourke@gmail.com>
2015-02-02 10:15:15 -0500

    changed analog_test and ldr.py. Run ldr.py for a live graph

--- analogTest/analog_test.c
+++ analogTest/analog_test.c
@@ -13,7 +13,7 @@ int16_t main(void) {
     init_timer();
     init_uart();
 
-    timer_setPeriod(&timer1, 0.5);
+    timer_setPeriod(&timer1, 0.01);
     timer_start(&timer1);
 
     int i;
@@ -21,21 +21,12 @@ int16_t main(void) {
         pin_analogIn(&A[i]);
     }
 
-    printf("Hello World!\n");
-
-    printf("What is your name? ");
-    uart_gets(&uart1, string, 40);
-    printf("Hello %s!\n", string);
-
-    printf("Printing analog voltages.\n");
-
     while (1) {
         if (timer_flag(&timer1)) {
             timer_lower(&timer1);
             // for (i = 0; i<6; i++) {
-                printf("%d\n", pin_read(&A[3]) >> 6);
+                printf("%d %d\n", pin_read(&A[3]) >> 6,pin_read(&A[2]) >> 6);
             // }
-            printf("\n");
         }
     }
 }
