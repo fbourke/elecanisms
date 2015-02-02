193f254a879231f15afbd0fe7b865fb3ab3d1f87
Forrest Bourke <forrestbourke@gmail.com>
2015-02-02 11:41:32 -0500

    changed analog_test so the pins are read every 1ms with a timer

--- analogTest/analog_test.c
+++ analogTest/analog_test.c
@@ -6,6 +6,12 @@
 #include <stdio.h>
 
 uint8_t string[40];
+volatile int pin3, pin4;
+
+void read_pins(_TIMER *self) {
+    pin3 = pin_read(&A[3]) >> 6;
+    pin4 = pin_read(&A[4]) >> 6;
+}
 
 int16_t main(void) {
     init_clock();
@@ -16,6 +22,9 @@ int16_t main(void) {
     timer_setPeriod(&timer1, 0.05);
     timer_start(&timer1);
 
+    timer_every(&timer2, 0.001, read_pins);
+
+
     int i;
     for (i = 0; i<6; i++) {
         pin_analogIn(&A[i]);
@@ -24,7 +33,7 @@ int16_t main(void) {
     while (1) {
         if (timer_flag(&timer1)) {
             timer_lower(&timer1);
-            printf("%d %d\n", pin_read(&A[3]) >> 6,pin_read(&A[2]) >> 6);
+            printf("%d %d\n", pin3,pin4);
         }
     }
 }
