2265e869706e12e053fba32a97ed84cc0c595422
Forrest Bourke <forrestbourke@gmail.com>
2015-02-02 12:55:45 -0500

    added velocity sensing

--- angleTest/angle_test.c
+++ angleTest/angle_test.c
@@ -8,27 +8,64 @@
 #include "uart.h"
 #include "ui.h"
 
+#define LOOP_FREQ 2000
+#define FLIP_THRESHOLD 700
+
 _PIN *sensorPosPin = &A[3];
 uint8_t string[40];
 
 // Position tracking variables
-int updatedPos = 0;     // keeps track of the latest updated value of the MR sensor reading
-int rawPos = 0;         // current raw reading from MR sensor
-int lastRawPos = 0;     // last raw reading from MR sensor
-int lastLastRawPos = 0; // last last raw reading from MR sensor
-int flipNumber = 0;     // keeps track of the number of flips over the 180deg mark
-int tempOffset = 0;
-int rawDiff = 0;
-int lastRawDiff = 0;
-int rawOffset = 0;
-int lastRawOffset = 0;
-const int flipThresh = 700;  // threshold to determine whether or not a flip over the 180 degree mark occurred
-int flipped = 0;
+volatile int updatedPos = 0;     // keeps track of the latest updated value of the MR sensor reading
+volatile int rawPos = 0;         // current raw reading from MR sensor
+volatile int lastRawPos = 0;     // last raw reading from MR sensor
+volatile int lastLastRawPos = 0; // last last raw reading from MR sensor
+volatile int flipNumber = 0;     // keeps track of the number of flips over the 180deg mark
+volatile int tempOffset = 0;
+volatile int rawDiff = 0;
+volatile int lastRawDiff = 0;
+volatile int rawOffset = 0;
+volatile int lastRawOffset = 0;
+volatile int flipped = 0;
+volatile int velocity = 0;
+volatile int acceleration = 0;
+
 
 void setup() {
     pin_analogIn(sensorPosPin);
     lastLastRawPos = pin_read(sensorPosPin);
     lastRawPos = pin_read(sensorPosPin);
+}
+
+void track_flips(_TIMER *self) {
+    // Get voltage output by MR sensor
+    rawPos = pin_read(sensorPosPin) >> 6;
+
+    // Calculate differences between subsequent MR sensor readings
+    rawDiff = rawPos - lastRawPos;
+    lastRawDiff = rawPos - lastLastRawPos;
+    rawOffset = abs(rawDiff);
+    lastRawOffset = abs(lastRawDiff);
+
+    // Update position record-keeping vairables
+    lastLastRawPos = lastRawPos;
+    lastRawPos = rawPos;
+
+    // Keep track of flips over 180 degrees
+    if((lastRawOffset > FLIP_THRESHOLD) && (flipped == 0)) { // enter this anytime the last offset is greater than the flip threshold AND it has not just flipped
+        if(lastRawDiff > 0) { flipNumber--; }
+        else { flipNumber++; }
+        flipped = 1;
+    }
+    else {
+        flipped = 0;
+    }
+
+    // Calculate speed
+    if(rawOffset < 150)
+    {
+        acceleration = (velocity - rawDiff*LOOP_FREQ)*LOOP_FREQ;
+        velocity = rawDiff*LOOP_FREQ;
+    }
 }
 
 int16_t main(void) {
@@ -42,44 +79,14 @@ int16_t main(void) {
     timer_setPeriod(&timer1, 0.05);
     timer_start(&timer1);
 
+    timer_every(&timer2, (1.0/LOOP_FREQ), track_flips);
+    printf("Control Loop Period: %f\n",1.0/LOOP_FREQ);
+
     while (1) {
-        // Get voltage output by MR sensor
-        rawPos = pin_read(sensorPosPin) >> 6;
-
-        // Calculate differences between subsequent MR sensor readings
-        rawDiff = rawPos - lastRawPos;
-        lastRawDiff = rawPos - lastLastRawPos;
-        rawOffset = abs(rawDiff);
-        lastRawOffset = abs(lastRawDiff);
-
-        // Update position record-keeping vairables
-        lastLastRawPos = lastRawPos;
-        lastRawPos = rawPos;
-
-        // Keep track of flips over 180 degrees
-        if((lastRawOffset > flipThresh) && (flipped == 0)) { // enter this anytime the last offset is greater than the flip threshold AND it has not just flipped
-            printf("rawDiff: %d, lastRawDiff: %d\n\n", rawDiff, lastRawDiff);
-            if(lastRawDiff > 0) { flipNumber--; }
-            else { flipNumber++; }
-
-            if(rawOffset > flipThresh) { // check to see if the data was good and the most current offset is above the threshold
-              updatedPos = rawPos + flipNumber*rawOffset; // update the pos value to account for flips over 180deg using the most current offset 
-              tempOffset = rawOffset;
-            }
-            else {                     // in this case there was a blip in the data and we want to use lastactualOffset instead
-              updatedPos = rawPos + flipNumber*lastRawOffset;  // update the pos value to account for any flips over 180deg using the LAST offset
-              tempOffset = lastRawOffset;
-            }
-            flipped = 1;
-        }
-        else {
-            updatedPos = rawPos + flipNumber*tempOffset; // need to update pos based on what most recent offset is 
-            flipped = 0;
-        }
-
         if (timer_flag(&timer1)) {
             timer_lower(&timer1);
-            printf("updatedPos: %d, flipNumber: %d\n", updatedPos, flipNumber);
+            printf("rawDiff:\t%d\tflipNumber:\t%d\tvelocity:\t%d\taccel:\t%d\n",
+                   rawDiff, flipNumber, velocity, acceleration);
         }
     }
 }
--- angleTestEric/angle_test_eric.c
+++ angleTestEric/angle_test_eric.c
@@ -7,6 +7,10 @@
 #include "timer.h"
 #include "uart.h"
 #include "ui.h"
+#include "oc.h"
+
+#define LOOP_FREQ 5000
+#define FLIP_THRESHOLD 700
 
 _PIN *sensorPosPin = &A[3];
 uint8_t string[40];
@@ -22,13 +26,19 @@ volatile int rawDiff = 0;
 volatile int lastRawDiff = 0;
 volatile int rawOffset = 0;
 volatile int lastRawOffset = 0;
-const int flipThresh = 700;  // threshold to determine whether or not a flip over the 180 degree mark occurred
 volatile int flipped = 0;
+volatile int velocity = 0;
+volatile int acceleration = 0;
+volatile int systemTime = 0;
+volatile int lastFlips = 0;
+
 
 void setup() {
     pin_analogIn(sensorPosPin);
     lastLastRawPos = pin_read(sensorPosPin);
     lastRawPos = pin_read(sensorPosPin);
+    setup_motor_shield();
+    pin_set(&D[6]);
 }
 
 void track_flips(_TIMER *self) {
@@ -46,7 +56,7 @@ void track_flips(_TIMER *self) {
     lastRawPos = rawPos;
 
     // Keep track of flips over 180 degrees
-    if((lastRawOffset > flipThresh) && (flipped == 0)) { // enter this anytime the last offset is greater than the flip threshold AND it has not just flipped
+    if((lastRawOffset > FLIP_THRESHOLD) && (flipped == 0)) { // enter this anytime the last offset is greater than the flip threshold AND it has not just flipped
         if(lastRawDiff > 0) { flipNumber--; }
         else { flipNumber++; }
         flipped = 1;
@@ -56,23 +66,44 @@ void track_flips(_TIMER *self) {
     }
 }
 
+void track_speed(_TIMER *self) {
+    systemTime++;
+    velocity = (flipNumber-lastFlips);
+    lastFlips = flipNumber;
+}
+
 int16_t main(void) {
     init_clock();
     init_pin();
     init_timer();
     init_uart();  // Default is 19200 baud
     init_ui();
+    init_oc();
     setup();
 
-    timer_setPeriod(&timer1, 0.05);
+    timer_setPeriod(&timer1, 0.1);
     timer_start(&timer1);
 
-    timer_every(&timer2, 0.0005, track_flips);
+    oc_pwm(&oc1, &D[3], NULL, 20000, 1023 << 6);  // D[3] is tri-stating
+
+
+    timer_every(&timer2, (1.0/LOOP_FREQ), track_flips);
+
+    timer_every(&timer3, 1, track_speed);
+
+    printf("Control Loop Period: %f\n",1.0/LOOP_FREQ);
 
     while (1) {
         if (timer_flag(&timer1)) {
             timer_lower(&timer1);
-            printf("rawDiff:\t%d\tlastRawDiff:\t%d\tflipNumber:\t%d\n", rawDiff, lastRawDiff, flipNumber);
+            printf("rawDiff:\t%d\tflipNumber:\t%d\tvelocity:\t%d\taccel:\t%d\n",
+                   rawDiff, flipNumber, velocity, acceleration);
+        }
+        if (!sw_read(&sw1)) {
+            pin_write(&D[3],386 << 6);
+        }
+        else {
+            pin_write(&D[3],1023 << 6);
         }
     }
 }
