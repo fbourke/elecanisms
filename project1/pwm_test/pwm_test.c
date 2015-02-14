40c033ce9fc15a144c9e053518e435500cedbb50
Eric Schneider <franz.eric.schneider@gmail.com>
2015-02-14 16:24:43 -0500

    Cosmetic changes to pwm_test

--- project1/pwm_test/pwm_test.c
+++ project1/pwm_test/pwm_test.c
@@ -1,4 +1,5 @@
 #include <p24FJ128GB206.h>
+#include <stdio.h>
 #include "config.h"
 #include "common.h"
 #include "oc.h"
@@ -6,7 +7,6 @@
 #include "timer.h"
 #include "uart.h"
 #include "ui.h"
-#include <stdio.h>
 
 uint8_t string[40];
 
@@ -26,8 +26,8 @@ int16_t main(void) {
     int duty_cycle = 0;
     // void oc_pwm(_OC *self, _PIN *out, _TIMER *timer,
     //             float freq (Hz), uint16_t duty)
-    // oc_pwm(&oc1, &D[3], NULL, 10000, duty_cycle << 6);  // D[3] is tri-stating
-    oc_pwm(&oc1, &D[6], NULL, 10000, duty_cycle << 6);  // D[6] is on/off
+    oc_pwm(&oc1, &D[3], NULL, 10000, duty_cycle << 6);  // D[3] is tri-stating
+    // oc_pwm(&oc1, &D[6], NULL, 10000, duty_cycle << 6);  // D[6] is on/off
 
     printf("Hello World!\n");
     led_on(&led1); led_on(&led2); led_on(&led3);
@@ -44,8 +44,8 @@ int16_t main(void) {
             duty_cycle += 128;
             if (duty_cycle > 1023) duty_cycle = 0;
             printf("Duty cycle: %d\n", duty_cycle);
-            // pin_write(&D[3], duty_cycle << 6);
-            pin_write(&D[6], duty_cycle << 6);
+            pin_write(&D[3], duty_cycle << 6);  // 1023 is stopped, 0 is full
+            // pin_write(&D[6], duty_cycle << 6);
         }
     }
 }
