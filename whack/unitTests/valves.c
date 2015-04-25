a9140baf3140db6c39ffd63924964643ee6e0573
JazminGonzalez-Rivero <jaz.gonzaro@gmail.com>
2015-04-25 13:12:24 -0400

    mole valves identified

--- whack/unitTests/valves.c
+++ whack/unitTests/valves.c
@@ -6,8 +6,13 @@
 #include "pin.h"
 #include "uart.h"
 
+typedef enum {
+    OPEN,
+    CLOSED
+} ValveState;
 
-uint16_t data = 7;
+
+uint16_t valveStates = 0;
 int count = 0;
 
 void delay(){
@@ -21,7 +26,7 @@ void longDelay(){
    int c = 1, d = 1;
 
    for ( c = 1 ; c <= 32000 ; c++ ){ 
-        for ( d = 1 ; d <= 32 ; d++ ){}
+        for ( d = 1 ; d <= 150 ; d++ ){}
     }
 }
 
@@ -39,6 +44,20 @@ void pulseClock(){
     pin_clear(&D[4]);
 }
 
+void writeValveState(int valveNumber, ValveState state) {
+    uint16_t bitMask;
+    if (state == OPEN) {
+        bitMask = 1<<valveNumber;
+        valveStates = valveStates | bitMask;
+    }
+    if (state == CLOSED) {
+        bitMask = ~(1<<valveNumber); 
+        valveStates = valveStates & bitMask;
+    }
+
+
+}
+
 int16_t main(void) {
     init_clock();
     init_ui();
@@ -48,20 +67,41 @@ int16_t main(void) {
     setup_pins();
     // Data = D3
     // Clock = D4
+  
+    // 0 Lefty in
+    // 1 Lefty out
+    // 2 Righty out
+    // 3 notWired
+    // 4 Orange out
+    // 5 Orange in
+    // 6 Righty in
+    // 7 unwired 
+
 
 
     while (1) {
         count = 0;
-/*        if (!sw_read(&sw1)) {
-            data = 255;
+        writeValveState(0, OPEN);
+        for(count = 0; count < 8; count++){
+            pin_write(&D[3],valveStates & (1<<count));
+            pulseClock();
         }
-        else {
-            data = 0;
-        }*/
-        data = 7;
-        for(count = 0; count < 6; count++){
-            pin_write(&D[3],data & 01);
-            data>>=1;
+        longDelay();
+        writeValveState(0, CLOSED);
+          for(count = 0; count < 8; count++){
+            pin_write(&D[3],valveStates & (1<<count));
+            pulseClock();
+        }
+        longDelay();
+                writeValveState(1, OPEN);
+        for(count = 0; count < 8; count++){
+            pin_write(&D[3],valveStates & (1<<count));
+            pulseClock();
+        }
+        longDelay();
+        writeValveState(1, CLOSED);
+          for(count = 0; count < 8; count++){
+            pin_write(&D[3],valveStates & (1<<count));
             pulseClock();
         }
         longDelay();
